cf578f9789595f3de8bb9a3cd1589b37dc4a2a35
Yura Evtushenko <y.evtushenko@nemotelecom.co.uk>
2015-08-06 23:21:27 +0700

    fixed dependency resolve

---
+++ src/context.ts
@@ -0,0 +1,75 @@
+/**
+ * Created by y.evtushenko on 06.08.15.
+ */
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="./DependencyConfig.ts" />
+
+import Dependency = require('./Dependency');
+import Q = require('q');
+
+class Context {
+
+    _dependencies: {};
+
+    constructor() {
+        this._dependencies = {};
+    }
+
+    dispose() {
+
+    }
+
+    register(config:DependencyConfig):void {
+        this.setDependency(config.id, new Dependency(config));
+    }
+
+    /**
+     * Promise of dependency object
+     * @param id
+     * @returns {Q.Promise<Dependency>[]}
+     */
+    get(id:string):Q.Promise<any> {
+        debugger;
+        return this.resolveDependencies([id])[0].then((service) => {
+            return service;
+        });
+    }
+
+    getDependency(id:string):Dependency {
+        return this._dependencies[id];
+    }
+
+    setDependency(id:string, dependency:Dependency) {
+        this._dependencies[id] = dependency;
+    }
+
+    hasDependency(id:string):boolean {
+        return this.getDependency(id) != null;
+    }
+
+    resolveDependencies(ids:string[]):Q.Promise<Dependency>[] {
+        return ids.map((id) => {
+            return this.resolveDependency(id);
+        });
+    }
+
+    resolveDependency(id):Q.Promise<any> {
+        var result;
+        var target = this.getDependency(id);
+        var dependencies = target.getConfig().dependencies;
+        var dependenciesPromises:Q.Promise<Dependency>[];
+
+        if (dependencies) {
+            dependenciesPromises = this.resolveDependencies(dependencies);
+            result = Q.all(dependenciesPromises).then((args) => {
+                return target.getService(...args);
+            });
+        } else {
+            result = target.getService();
+        }
+
+        return Q.resolve(result);
+    }
+}
+
+export = Context
